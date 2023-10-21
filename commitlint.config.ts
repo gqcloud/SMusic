a5caaa547caa1a1dc3821f653deab79759b14d50
gqcloud <874310491@qq.com>
2023-10-21 14:18:26 +0800

    feat: init project

---
+++ commitlint.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {},
+  prompt: {
+    useEmoji: false,
+    emojiAlign: 'center',
+    allowCustomIssuePrefixs: false,
+    allowEmptyIssuePrefixs: false
+  }
+}
